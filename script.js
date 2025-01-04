abb64031c4545b7a38824ebe608c2a9b634d9ed0
Javier <J_sedanomar@uncg.edu>
2025-01-04 12:28:42 -0500

    created all the math functions for the calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+function addNums (x,y){
+return x + y;
+}
+
+function subtractNums (x,y){
+    return x - y;
+}
+
+function multiplyNums (x,y){
+    return x * y;
+}
+
+function divideNums (x,y){
+    return x / y;
+}
+
+function convertToDecimal(num){
+   return num / 100;
+}
+
+function convertToNegative(num){
+    return num * -1;
+}
