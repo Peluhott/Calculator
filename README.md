e9b5fbbebde8947c531a790dba40b61ac211e751
Javier <J_sedanomar@uncg.edu>
2025-01-04 23:09:45 -0500

    added the variables to hold operator and numbers and result, added a switch case, added a even listener for each button

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ buttons i'm going to add
 -buttons for 0-9
 
 put numbbers in an array so it can display it as you type
+
+
+add the numbers to an array to display
+
+when an operation is hit , takes the numbers in array, join them together, and parse them as a double, check if it has a negative sign in front if it does, remove it from the array, parse it and then multiply it by negative 1
+
+when the opeeration is hit it stores that operator in a string, it checks first if it is empty, if it is not empty, it takes the amount that was already parsed, parses the array that just was typed then and calls the function on that, displays that number. then it stores that number and emptys out the other array list
--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let displayNums = []
+let firstVariable;
+let secondVariable;
+let operatorVariable;
+let result;
+
 function addNums (x,y){
 return x + y;
 }
@@ -21,3 +27,32 @@ function convertToDecimal(num){
 function convertToNegative(num){
     return num * -1;
 }
+
+function evaluate(string){
+    switch(string){
+        case '+':
+        result = addNums(firstVariable, secondVariable);
+            break;
+    
+
+        case '-':
+        result = subtractNums(firstVariable, secondVariable);
+            break;
+    
+        case '*':
+        result = multiplyNums(firstVariable, secondVariable);
+            break;
+
+        case '/':
+        result = divideNums(firstVariable, secondVariable);
+            break;
+    }
+
+}
+const buttonInput =  document.querySelector('button');
+buttonInput.addEventListener("click", (event) => {
+    const buttonValue = event.target.innerText;
+    if(buttonValue == "="){
+        evaluate(buttonValue);
+    }
+});
